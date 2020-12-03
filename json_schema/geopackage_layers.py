e58bd168dddb50b45677df877964d2d7e5ef72c4
Carlos H Brandt <c.brandt@jacobs-university.de>
2020-12-03 18:40:43 +0100

    Update layers schmea to use object/properties and allow extra columns

--- json_schema/geopackage_layers.py
+++ json_schema/geopackage_layers.py
@@ -1,3 +1,4 @@
+import gpt
 
 _component = __file__.split('.')[0]
 _schemafile = '.'.join([_component, 'schema', 'json'])
@@ -7,8 +8,12 @@ with open(_schemafile) as f:
     schema = json.load(f)
 
 def validate(gpkg_path):
-    import fiona
-    data = fiona.listlayers(gpkg_path)
+    # import fiona
+    # data = fiona.listlayers(gpkg_path)
+    gpkg = gpt.read_file(gpkg_path)
+
+    # This is a workaround during devel, until I understand how to tell jsonschema about the DataFrames
+    data = {ln:ln for ln in gpkg.list()}
 
     import jsonschema
     res = jsonschema.validate(data, schema)
