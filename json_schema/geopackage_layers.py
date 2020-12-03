205d8a77fdddcf35d129aece22fe6c48c4916f18
Carlos H Brandt <c.brandt@jacobs-university.de>
2020-12-03 21:55:23 +0100

    [wip] add some structure to layer_styles (temporary)

--- json_schema/geopackage_layers.py
+++ json_schema/geopackage_layers.py
@@ -12,8 +12,17 @@ def validate(gpkg_path):
     # data = fiona.listlayers(gpkg_path)
     gpkg = gpt.read_file(gpkg_path)
 
+    # print("\nGeoPackage layers/columns:")
+    # for name,table in gpkg.layers:
+    #     print(name)
+    #     print(table.columns)
+    #     print()
+
     # This is a workaround during devel, until I understand how to tell jsonschema about the DataFrames
-    data = {ln:ln for ln in gpkg.list()}
+    data = {ln:{} for ln in gpkg.list()}
+    data['layer_styles']['geometry'] = list(gpkg['layer_styles']['geometry'].values)
+    data['layer_styles']['geometry'] = []
+    # print(data)
 
     import jsonschema
     res = jsonschema.validate(data, schema)
